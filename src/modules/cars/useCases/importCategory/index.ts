edce4168292b86db315a494103d00c883fa294a9
Joao Marcos <joomarcos.belanga98@gmail.com>
2021-12-06 16:32:35 -0300

    saving datas of import

--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,8 +1,11 @@
+import { CategoriesRepository } from "../../repositories/implementation/CategoriesRepository";
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
 
-const importCategoryUseCase = new ImportCategoryUseCase();
+const categoriesRepository = CategoriesRepository.getInstace();
+
+const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
 
 const importCategoryController = new ImportCategoryController(importCategoryUseCase);
 
