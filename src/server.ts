c450dd0a51b35ae3d446c25635f6b008afad2bc0
Joao Marcos <joomarcos.belanga98@gmail.com>
2021-12-07 10:14:19 -0300

    Creating swagger.json

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
-/**@name Dependecies */
+/** @name Dependecies */
 import express from "express";
-/**@name Routes */
+import swaggerUi from "swagger-ui-express";
+/** @name Routes */
 import { router } from "./routes";
+/** @name JSON */
+import swaggerFile from "./swagger.json";
 
 
 const app = express();
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.use(express.json());
 
-app.use(router)
+app.use(router);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000! 🚀")
-})
+});
