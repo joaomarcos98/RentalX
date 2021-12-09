67ae5beea2cc142e8effe65db59bfc9212dfec2b
Joao Marcos <joomarcos.belanga98@gmail.com>
2021-12-09 15:59:53 -0300

    Adding docker

--- src/server.ts
+++ src/server.ts
@@ -16,5 +16,5 @@ app.use(express.json());
 app.use(router);
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000! 🚀")
+    console.log("Server is running on port 3000! 🚀");
 });
