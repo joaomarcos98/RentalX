d968040e7283809c1a6bd95cd0765a4f50c647d3
Joao Marcos <joomarcos.belanga98@gmail.com>
2021-12-20 15:15:32 -0300

    Creating a new route: Create User

---
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -0,0 +1,30 @@
+import { container } from "tsyringe";
+import { Request, Response } from "express";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+class CreateUserController {
+
+    async handle(req: Request, res: Response): Promise<Response> {
+        const {
+            name,
+            username,
+            email,
+            password,
+            driver_license
+        } = req.body;
+
+        const createUserUseCase = container.resolve(CreateUserUseCase);
+
+        await createUserUseCase.execute({
+            name,
+            username,
+            email,
+            password,
+            driver_license
+        })
+
+        return res.status(201).send();
+    }
+}
+
+export { CreateUserController };
